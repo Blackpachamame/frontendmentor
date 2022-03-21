4efa0de17b4c7279219e48036f5073faabbad3c3
Marcos Travaglini <marcos.travaglini@est.fi.uncoma.edu.ar>
2022-03-21 01:08:58 -0300

    Desafío Ping coming soon page

---
+++ desafíos-frontendmentor/ping-coming-soon-page/js/app.js
@@ -0,0 +1,29 @@
+const notifyForm = document.querySelector("form");
+const inputField = document.querySelector("#email");
+
+notifyForm.addEventListener("submit", (e) => {
+    const emailInput = inputField.value;
+    const errorMessage = document.querySelector(".error-message");
+    const emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+    if (emailInput === "" || emailInput === null) {
+        errorMessage.style.display = "block";
+        inputField.classList.add("error-border");
+        errorMessage.innerHTML =
+            "Whoops! It looks like you forgot to add your email";
+        inputField.setAttribute("aria-invalid", "true");
+        inputField.setAttribute("aria-describedBy", "error");
+    } else if (!emailInput.match(emailRegex)) {
+        errorMessage.style.display = "block";
+        inputField.classList.add("error-border");
+        errorMessage.innerHTML = "Please provide a valid email address";
+        inputField.setAttribute("aria-invalid", "true");
+        inputField.setAttribute("aria-describedBy", "error");
+    } else {
+        errorMessage.style.display = "none";
+        inputField.classList.remove("error-border");
+        inputField.removeAttribute("aria-invalid");
+        inputField.removeAttribute("aria-describedBy");
+        inputField.value = "";
+    }
+    e.preventDefault();
+});
