17434c9e4d3d85045f2d89bf1774adc2c397f371
Marcos Travaglini <marcos.travaglini@est.fi.uncoma.edu.ar>
2022-07-01 00:55:12 -0300

    Desafío Base Apparel coming soon page: Completo, falta README.

--- desafíos-frontendmentor/base-apparel-coming-soon/js/app.js
+++ desafíos-frontendmentor/base-apparel-coming-soon/js/app.js
@@ -7,20 +7,20 @@ notifyForm.addEventListener("submit", (e) => {
     const emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
     if (emailInput === "" || emailInput === null) {
         errorMessage.style.display = "block";
-        inputField.classList.add("error-border");
+        inputField.classList.add("form__input-error");
         errorMessage.innerHTML =
             "Whoops! It looks like you forgot to add your email";
         inputField.setAttribute("aria-invalid", "true");
         inputField.setAttribute("aria-describedBy", "error");
     } else if (!emailInput.match(emailRegex)) {
         errorMessage.style.display = "block";
-        inputField.classList.add("error-border");
+        inputField.classList.add("form__input-error");
         errorMessage.innerHTML = "Please provide a valid email";
         inputField.setAttribute("aria-invalid", "true");
         inputField.setAttribute("aria-describedBy", "error");
     } else {
         errorMessage.style.display = "none";
-        inputField.classList.remove("error-border");
+        inputField.classList.remove("form__input-error");
         inputField.removeAttribute("aria-invalid");
         inputField.removeAttribute("aria-describedBy");
         inputField.value = "";
