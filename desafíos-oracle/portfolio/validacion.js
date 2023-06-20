794b7e6c00db57c86bbd2b21744daa6adaaa0830
Marcos Travaglini <marcos.travaglini@est.fi.uncoma.edu.ar>
2023-06-20 20:44:45 -0300

    Creando variables al script

--- desafíos-oracle/portfolio/validacion.js
+++ desafíos-oracle/portfolio/validacion.js
@@ -1,1 +1,4 @@
-//Haz tú validación en javascript acá
+const NOMBRE = document.getElementById('nombre')
+const EMAIL = document.getElementById('email')
+const ASUNTO = document.getElementById('asunto')
+const MENSAJE = document.getElementById('mensaje')
