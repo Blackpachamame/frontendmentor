5cdbf45fb098d5ce36e66cc0d6ff6b82b4211733
Marcos Travaglini <marcos.travaglini@est.fi.uncoma.edu.ar>
2023-06-25 03:42:04 -0300

    Validaciones form portafolio completas, faltan ajustes.

--- desafíos-oracle/portfolio/assets/js/app.js
+++ desafíos-oracle/portfolio/assets/js/app.js
@@ -1,8 +1,15 @@
 import { valida } from "./validacion.js";
 
 const inputs = document.querySelectorAll("input");
+const textareas = document.querySelectorAll("textarea");
 
 inputs.forEach((input) => {
+    input.addEventListener("blur", (input) => {
+        valida(input.target);
+    });
+});
+
+textareas.forEach((input) => {
     input.addEventListener("blur", (input) => {
         valida(input.target);
     });
