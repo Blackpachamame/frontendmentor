35ca39f195495b8c8f9d92db643924371cf51711
Marcos Travaglini <marcos.travaglini@est.fi.uncoma.edu.ar>
2023-07-20 14:05:16 -0300

    Agregando producto

--- desafíos-oracle/alurageek/controller/agregarProducto-controller.js
+++ desafíos-oracle/alurageek/controller/agregarProducto-controller.js
@@ -8,11 +8,6 @@ form.addEventListener('submit', (evento) => {
     const precio = document.getElementById("precio").value;
     const descripcion = document.getElementById("descripcion").value;
     const categoria = document.getElementById("categoria").value;
-    console.log(categoria)
-
-    // const [selectedCategory] = [].filter
-    //     .call(categoria.options, option => option.selected)
-    //     .map(option => option.text);
 
     productServices.crearProducto(url, categoria, nombre, precio, descripcion).then(respuesta => {
         alert('El PRODUCTO FUE CREADO CON EXITO')
@@ -27,45 +22,45 @@ form.addEventListener('submit', (evento) => {
 
 
 // Validaciones
-export function valida(input) {
-    const tipoDeInput = input.dataset.tipo;
-    if (input.validity.valid) {
-        input.parentElement.classList.remove("input-container--invalid");
-    } else {
-        input.parentElement.classList.add("input-container--invalid");
-        input.parentElement.querySelector(".input-message-error").innerHTML =
-            mostrarMensajeDeError(tipoDeInput, input);
-    }
-}
+// export function valida(input) {
+//     const tipoDeInput = input.dataset.tipo;
+//     if (input.validity.valid) {
+//         input.parentElement.classList.remove("input-container--invalid");
+//     } else {
+//         input.parentElement.classList.add("input-container--invalid");
+//         input.parentElement.querySelector(".input-message-error").innerHTML =
+//             mostrarMensajeDeError(tipoDeInput, input);
+//     }
+// }
 
 
-const tipoDeErrores = [
-    "valueMissing",
-    "typeMismatch",
-];
+// const tipoDeErrores = [
+//     "valueMissing",
+//     "typeMismatch",
+// ];
 
-const mensajesDeError = {
-    nombre: {
-        valueMissing: "Debe ingresar un nombre",
-    },
-    email: {
-        valueMissing: "Debe ingresar un email",
-        typeMismatch: "El email no es válido",
-    },
-    asunto: {
-        valueMissing: "Este campo no puede estar vacío",
-    },
-    mensaje: {
-        valueMissing: "Este campo no puede estar vacío",
-    },
-};
+// const mensajesDeError = {
+//     nombre: {
+//         valueMissing: "Debe ingresar un nombre",
+//     },
+//     email: {
+//         valueMissing: "Debe ingresar un email",
+//         typeMismatch: "El email no es válido",
+//     },
+//     asunto: {
+//         valueMissing: "Este campo no puede estar vacío",
+//     },
+//     mensaje: {
+//         valueMissing: "Este campo no puede estar vacío",
+//     },
+// };
 
-function mostrarMensajeDeError(tipoDeInput, input) {
-    let mensaje = "";
-    tipoDeErrores.forEach((error) => {
-        if (input.validity[error]) {
-            mensaje = mensajesDeError[tipoDeInput][error];
-        }
-    });
-    return mensaje;
-}
+// function mostrarMensajeDeError(tipoDeInput, input) {
+//     let mensaje = "";
+//     tipoDeErrores.forEach((error) => {
+//         if (input.validity[error]) {
+//             mensaje = mensajesDeError[tipoDeInput][error];
+//         }
+//     });
+//     return mensaje;
+// }
