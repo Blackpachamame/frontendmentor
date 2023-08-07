d474b3ad216ec5cea99d021c7655b15039df9a24
Marcos Travaglini <marcos.travaglini@est.fi.uncoma.edu.ar>
2023-08-07 03:09:25 -0300

    Creando usuarioService

---
+++ desafíos-oracle/alurageek/service/usuarioService.js
@@ -0,0 +1,58 @@
+const listaUsuarios = () =>
+    fetch('https://ds-json.herokuapp.com/users').then(respuesta => respuesta.json());
+
+const crearUsuario = (name, email, password) => {
+    return fetch('https://ds-json.herokuapp.com/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            name,
+            email,
+            password,
+            id: uuid.v4()
+        })
+    })
+        .then(respuesta => {
+            if (respuesta.ok) {
+                return respuesta.body
+            }
+            throw new Error('No fue posible crear un usuario')
+        });
+};
+
+const detalleUsuario = (id) => {
+    return fetch(`https://ds-json.herokuapp.com/users/${id}`).then((resp) => resp.json());
+};
+
+const eliminarUsuario = (id) => {
+    return fetch(`https://ds-json.herokuapp.com/users/${id}`, {
+        method: 'DELETE',
+    });
+};
+
+const editarUsuario = (name, email, password, id) => {
+    return fetch(`https://ds-json.herokuapp.com/users/${id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            name,
+            email,
+            password,
+        })
+    })
+        .then(response => response)
+        .catch(error => console.error(error))
+};
+
+export const userServicios = {
+    listaUsuarios,
+    crearUsuario,
+    eliminarUsuario,
+    detalleUsuario,
+    editarUsuario,
+}
+
