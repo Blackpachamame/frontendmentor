4220447a03a553f13439ebfb6d47aa4756cd4e94
Marcos Travaglini <marcos.travaglini@est.fi.uncoma.edu.ar>
2023-08-16 03:04:25 -0300

    - Creamos componentes funcionales. - Agregamos estilos css a nuestros componentes. - Instalamos React Developer Tools para poder inspeccionar nuestros componentes.

--- proyectos-prueba/react/org/src/App.js
+++ proyectos-prueba/react/org/src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hola Mundo
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aprende React
-        </a>
-      </header>
+    <div>
+      <Header />
     </div>
   );
 }
