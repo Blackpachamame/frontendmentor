5af4f59824720651178737df7bb1be6651b79259
Marcos Travaglini <marcos.travaglini@est.fi.uncoma.edu.ar>
2023-08-23 04:40:31 -0300

    Rutas anidadas

--- proyectos-prueba/react/doguito-app/README.md
+++ proyectos-prueba/react/doguito-app/README.md
@@ -29,6 +29,9 @@ Cuando esté listo para implementar en producción, cree un paquete minimizado c
 - Hicimos una requisición de un artículo específico pasando el id del curso utilizando `useEffect`.
 - Mandamos el usuario para el componente 404 utilizando la función `useNavigate()`.
 - Mejoramos la experiencia de buscar un artículo creando un filtro por categorías.
+- Trabajamos con `rutas anidadas`.
+- Vimos cómo crear una segunda camada de filtros para mostrar artículos de una subcategoría específica, mejorando la experiencia del usuario al buscar un artículo.
+- Usamos el hook `useResolvedPath` para crear caminos dinámicos.
 
 ## Recursos útiles
 
--- proyectos-prueba/react/doguito-app/src/App.js
+++ proyectos-prueba/react/doguito-app/src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import Sobre from './pages/Sobre';
 import Page404 from './pages/Page404';
 import Header from './components/Header';
+import Categoria from './pages/Categoria';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Post from './pages/Post';
 
@@ -16,6 +17,7 @@ function App() {
                 <Route path='/' element={<Home />} />
                 <Route path='/sobre' element={<Sobre />} />
                 <Route path='/posts/:id' element={<Post />} />
+                <Route path='/categoria/:id/*' element={<Categoria />} />
                 <Route path='*' element={<Page404 />} />
             </Routes>
         </Router>
