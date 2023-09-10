8497c84080de9f511bfdc1fdb2cd6da12fdc707e
Marcos Travaglini <marcos.travaglini@est.fi.uncoma.edu.ar>
2023-09-10 00:14:19 -0300

    Creando theme

--- desafíos-oracle/aluraflix/src/App.jsx
+++ desafíos-oracle/aluraflix/src/App.jsx
@@ -1,11 +1,13 @@
+import { ThemeProvider } from "styled-components";
 import GlobalStyle from "./GlobalStyle";
+import { theme } from "./Theme";
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyle />
       <h1>Hola Mundo</h1>
-    </>
+    </ThemeProvider>
   );
 }
 
---
+++ desafíos-oracle/aluraflix/src/Theme.jsx
@@ -0,0 +1,12 @@
+export const theme = {
+  colors: {
+    primary: "#000",
+    secondary: "#fff",
+  },
+  sizes: {
+    xs: "4px",
+    s: "8px",
+    m: "12px",
+    xl: "16px",
+  },
+};
