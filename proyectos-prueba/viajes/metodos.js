4e430022afd11aadc39cefc89e9d513a510e5067
Marcos Travaglini <marcos.travaglini@est.fi.uncoma.edu.ar>
2023-12-30 15:55:02 -0300

    Quitando proyectos prueba de este repositorio

--- proyectos-prueba/viajes/metodos.js
+++
@@ -1,6 +0,0 @@
-const btn = document.getElementsByClassName('btn-menu')[0]
-const navbarItems = document.getElementsByClassName('navbar-items')[0]
-
-btn.addEventListener('click', () => {
-    navbarItems.classList.toggle('active')
-})
