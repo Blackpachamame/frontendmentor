06df8a1001c752111bd5e7742a24c07872d7804f
Marcos Travaglini <marcos.travaglini@est.fi.uncoma.edu.ar>
2024-01-29 01:47:27 -0300

    fix: ajustes en las validaciones de la age calculator app, ahora muestra mensajes personalizados según el error en el input

--- junior/age-calculator-app/js/scripts.js
+++ junior/age-calculator-app/js/scripts.js
@@ -9,6 +9,9 @@ formulario.addEventListener("submit", (e) => {
     let dayInput = document.getElementById('day').value;
     let monthInput = document.getElementById('month').value;
     let yearInput = document.getElementById('year').value;
+    resultDays.textContent = '--';
+    resultMonths.textContent = '--';
+    resultYears.textContent = '--';
     exibeErro('This field is required', dayInput, monthInput, yearInput);
 });
 
@@ -34,29 +37,47 @@ function exibeErro(mensagem, dayInput, monthInput, yearInput) {
 }
 
 function validarData(dayInput, monthInput, yearInput) {
-    const mensagem = 'Valor inválido';
+    let mensagem = 'Must be a valid date';
 
     if (monthInput < 1 || monthInput > 12) {
+        mensagem = 'Must be a valid month';
         document.getElementById('error__month').textContent = mensagem;
     }
     else if ((monthInput == 1 || monthInput == 3 || monthInput == 5 || monthInput == 7 || monthInput == 8 || monthInput == 10 || monthInput == 12) && (dayInput < 1 || dayInput > 31)) {
+        mensagem = 'Must be a valid day';
         document.getElementById('error__day').textContent = mensagem;
     }
-    else if ((monthInput == 4 || monthInput == 6 || monthInput == 9 || monthInput == 11) && (dayInput < 1 || dayInput > 30)) {
-        document.getElementById('error__day').textContent = mensagem;
+    else if ((monthInput == 4 || monthInput == 6 || monthInput == 9 || monthInput == 11) && (dayInput < 1 || dayInput > 31)) {
+        if (dayInput === 31) {
+            document.getElementById('error__day').textContent = mensagem;
+        } else {
+            mensagem = 'Must be a valid day';
+            document.getElementById('error__day').textContent = mensagem;
+        }
     }
     else if (monthInput == 2) {
         if ((yearInput % 4 == 0 && yearInput % 100 != 0) || yearInput % 400 == 0) {
-            if (dayInput < 1 || dayInput > 29) {
+            if (dayInput < 1 || dayInput > 31) {
+                document.getElementById('error__day').textContent = mensagem;
+            } else if (dayInput > 29) {
+                mensagem = 'Must be a valid day';
                 document.getElementById('error__day').textContent = mensagem;
             }
         } else {
-            if (dayInput < 1 || dayInput > 28) {
+            if (dayInput < 1 || dayInput > 31) {
+                document.getElementById('error__day').textContent = mensagem;
+            } else if (dayInput > 28) {
+                mensagem = 'Must be a valid day';
                 document.getElementById('error__day').textContent = mensagem;
             }
         }
     }
-    else if (yearInput < 0 || yearInput > data.getFullYear()) {
+    else if (yearInput < 0) {
+        mensagem = 'Must be a valid year';
+        document.getElementById('error__year').textContent = mensagem;
+    }
+    else if (yearInput > data.getFullYear()) {
+        mensagem = 'Must be in the past';
         document.getElementById('error__year').textContent = mensagem;
     }
     else {
